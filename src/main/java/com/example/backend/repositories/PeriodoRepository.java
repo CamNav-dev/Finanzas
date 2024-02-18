0f77c1a3c6cc79ede18a6d84f513a3ba83ebb783
Arturo Rojas <joserojasr23@gmail.com>
2024-02-18 15:28:50 -0500

    Update PeriodoRepository.java
    cambio a long

--- src/main/java/com/example/backend/repositories/PeriodoRepository.java
+++ src/main/java/com/example/backend/repositories/PeriodoRepository.java
@@ -3,5 +3,5 @@ package com.example.backend.repositories;
 import com.example.backend.entities.periodo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PeriodoRepository extends JpaRepository<periodo, Integer> {
+public interface PeriodoRepository extends JpaRepository<periodo, Long> {
 }
